f568e2843a431f9f30b1c3a1ac0fb71c9478e808
Renato Lui Geh <renatogeh@gmail.com>
2021-05-28 18:53:53 -0300

    Rewrite 20-datasets script

--- experiments/run.jl
+++ experiments/run.jl
@@ -3,39 +3,37 @@ using RPCircuits
 using Random
 using Serialization
 
-datasets = ["nltcs", "book", "plants", "baudio", "jester", "bnetflix", "accidents", "mushrooms",
-            "adult", "dna"]
+default_value(i, x, f) = length(ARGS) < i ? x : f(ARGS[i])
+
+name = ARGS[1]
+t_proj = Symbol(ARGS[2])
+n_projs = parse(Int, ARGS[3])
+is_mix = parse(Bool, ARGS[4])
+max_height = parse(Int, ARGS[5])
+trials = parse(Int, ARGS[6])
+batchsize = default_value(7, 500, x -> parse(Int, x))
+em_steps = default_value(8, 50, x -> parse(Int, x))
+
+datasets = ["nltcs", "book", "plants", "baudio", "jester", "bnetflix", "accidents", "dna"]
 LL = Vector{Float64}(undef, length(datasets))
 for data_idx ∈ 1:length(datasets)
   println("Dataset: ", datasets[data_idx])
   R, V, T = twenty_datasets(datasets[data_idx]; as_df = false)
   println("Learning structure...")
-  C = learn_projections(R; n_projs = 3, t_proj = :max, binarize = true, no_dist = true, single_mix = false)
+  C = learn_projections(R; n_projs = n_projs, t_proj = t_proj, binarize = true, single_mix = is_mix,
+                        max_height = max_height, trials = trials)
   println("Learning parameters...")
   learner = SEM(C)
-  batchsize = 500
-  # avgnll = 0.0
-  # runnll = 0.0
-  # println("It: $(learner.steps) \t train NLL: $avgnll \t held-out NLL: $(NLL(C, V))")
   indices = shuffle!(collect(1:size(R,1)))
-  while !converged(learner) && learner.steps < 30
+  while !converged(learner) && learner.steps < em_steps
     sid = rand(1:(length(indices)-batchsize))
     batch = view(R, indices[sid:(sid+batchsize-1)], :)
     η = 0.975^learner.steps #max(0.95^learner.steps, 0.3)
     update(learner, batch, η)
     println("Iteration: ", learner.steps)
-    # testnll = NLL(C, V)
-    # batchnll = NLL(C, batch)
-    # running average NLL
-    # avgnll *= (learner.steps-1)/learner.steps # discards initial NLL
-    # avgnll += batchnll/learner.steps
-    # runnll = (1-η)*runnll + η*batchnll
-    # println("It: $(learner.steps) \t avg NLL: $avgnll \t mov NLL: $runnll \t batch NLL: $batchnll \t held-out NLL: $testnll \t Learning rate: $η")
   end
   LL[data_idx] = -NLL(C, T)
   println("LL: ", LL[data_idx])
-  println("Saving results...")
-  serialize("$(datasets[data_idx])_multi_max.data", LL[data_idx])
-  println("Saving circuit...")
-  save(C, "saved/sid/$(datasets[data_idx])_multi_max.spn")
 end
+println(LL)
+serialize("results/$(name).data", LL)
