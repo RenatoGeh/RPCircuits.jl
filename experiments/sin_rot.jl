248187604a27072a2b80a5128d1a159b3109efe0
denismaua <denis.maua@gmail.com>
2021-06-03 16:58:58 -0300

    some changes for sin_rot

--- experiments/sin_rot.jl
+++ experiments/sin_rot.jl
@@ -1,18 +1,20 @@
 using Pkg; Pkg.activate("..")
 using RPCircuits
 using NPZ
-using ThreadPools
+#susing ThreadPools
 using Random
 
-function learn_parameters!(C::Circuit, R, V; batchsize = 500)
+Random.seed!(3)
+
+function learn_parameters!(C::Circuit, R, V; batchsize = 30)
   learner = SEM(C)
   avgnll = 0.0
   runnll = 0.0
   indices = shuffle!(collect(1:size(R,1)))
-  while !converged(learner) && learner.steps < 100
+  while !converged(learner) && learner.steps < 20
     sid = rand(1:(length(indices)-batchsize))
-    batch = view(R, indices[sid:(sid+batchsize-1)], :)
-    η = 0.975^learner.steps #max(0.95^learner.steps, 0.3)
+    batch = R #view(R, indices[sid:(sid+batchsize-1)], :)
+    η = 1.0 #^learner.steps #max(0.95^learner.steps, 0.3)
     update(learner, batch, η)
     testnll = NLL(C, V)
     batchnll = NLL(C, batch)
@@ -26,23 +28,25 @@ function learn_parameters!(C::Circuit, R, V; batchsize = 500)
 end
 
 D = npzread("sin_rot.npy")
-R, V, T = D[1:end-151,:], D[end-150:end-101,:], D[end-100:end,:]
+indices = shuffle!(collect(1:size(D,1)))
+R, V, T = D[indices[1:(end-151)],:], D[indices[end-150:end-101],:], D[indices[end-100:end],:]
 
 println("Learning...")
 L_f = [
-       () -> learn_projections(D; n_projs = 3, min_examples = 5, max_height = 15, t_proj = :sid, binarize = false, no_dist = true, c = 100.0),
-       () -> learn_projections(D; min_examples = 5, max_height = 100, n_projs = 25, t_proj = :sid, binarize = false, no_dist = true, single_mix = true, c = 100.0),
-       () -> learn_projections(D; n_projs = 3, min_examples = 5, max_height = 15, t_proj = :max, binarize = false, no_dist = true, r = 1.0, c = 100.0),
-       () -> learn_projections(D; min_examples = 5, max_height = 100, n_projs = 25, t_proj = :max, binarize = false, no_dist = true, r = 1.0, single_mix = true, c = 100.0),
+       () -> learn_projections(R; n_projs = 3, min_examples = 5, max_height = 15, t_proj = :sid, binarize = false, no_dist = true, c = 100.0),
+       () -> learn_projections(R; min_examples = 5, max_height = 100, n_projs = 25, t_proj = :sid, binarize = false, no_dist = true, single_mix = true, c = 100.0),
+       () -> learn_projections(R; n_projs = 3, min_examples = 5, max_height = 15, t_proj = :max, binarize = false, no_dist = true, r = 1.0, c = 100.0),
+       () -> learn_projections(R; min_examples = 5, max_height = 100, n_projs = 25, t_proj = :max, binarize = false, no_dist = true, r = 1.0, single_mix = true, c = 100.0),
       ]
 
 load_from_file = false
 
 names = ["sid", "sid_single", "max", "max_single"]
-if load_from_file C_all = Circuit.("saved/sin_rot/" .* names .* ".spn")
+if load_from_file C_all = Circuit.("saved/sin_rot/" .* names .* ".pc")
 else
   C_all = Vector{Circuit}(undef, length(names))
-  @qthreads for i ∈ 1:length(C_all)
+  #@qthreads 
+  for i ∈ 1:length(C_all)
     println("Learning structure...")
     C = L_f[i]()
     println("Learning parameters...")
@@ -52,7 +56,7 @@ else
     open("results/sin_rot/$(names[i]).txt", "w") do out write(out, string(ll)) end
     C_all[i] = C
     println("Saving to file...")
-    save(C, "saved/sin_rot/" * names[i] * ".spn")
+    save(C, "saved/sin_rot/" * names[i] * ".pc")
   end
 end
 x_bounds, y_bounds = -1.5:0.05:1.5, -2.0:0.05:2.0
