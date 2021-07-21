7338897d39082ad647532094d392aa6835dad1e0
Renato Lui Geh <renatogeh@gmail.com>
2021-07-21 13:59:33 -0300

    Structured learning experiments

--- experiments/run.jl
+++ experiments/run.jl
@@ -3,6 +3,11 @@ using RPCircuits
 using Random
 using Serialization
 
+using BlossomV
+using ProbabilisticCircuits: learn_vtree
+using LogicCircuits: Vtree
+using DataFrames
+
 default_value(i, x, f) = length(ARGS) < i ? x : f(ARGS[i])
 
 name = ARGS[1]
@@ -16,6 +21,8 @@ em_steps = default_value(8, 100, x -> parse(Int, x))
 full_em_steps = em_steps + default_value(9, 30, x -> parse(Int, x))
 learnspn_style = default_value(10, false, x -> parse(Bool, x))
 dense_leaves = default_value(11, false, x -> parse(Bool, x))
+split_t = default_value(12, :random, x -> Symbol(x))
+residuals = default_value(13, false, x -> parse(Bool, x))
 
 function count_nodes(C::Circuit, D::AbstractMatrix{<:Real})::Tuple{Int, Int, Int}
   sums, prods, leaves = 0, 0, size(D, 2)*2
@@ -29,8 +36,13 @@ end
 tee(out, str) = (write(out, str * "\n"); println(str))
 verbose = false
 
+all_data = ["accidents", "ad", "baudio", "bbc", "bnetflix", "book", "c20ng", "cr52", "cwebkb",
+            "dna", "jester", "kdd", "kosarek", "msnbc", "msweb", "nltcs", "plants", "pumsb_star",
+            "tmovie", "tretail"]
+
 function run_reg_em()
-  datasets = ["nltcs", "book", "plants", "baudio", "jester", "bnetflix", "accidents", "dna"]
+  # datasets = ["nltcs", "book", "plants", "baudio", "jester", "bnetflix", "accidents", "dna"]
+  datasets = all_data
   LL = Vector{Float64}(undef, length(datasets))
   S = Vector{Tuple{Int, Int, Int}}(undef, length(datasets))
   for data_idx ∈ 1:length(datasets)
@@ -38,16 +50,23 @@ function run_reg_em()
     R, V, T = twenty_datasets(datasets[data_idx]; as_df = false)
     println("Learning structure...")
     if learnspn_style
+      if split_t == :vtree println("  Learning vtree..."); vtree = learn_vtree(DataFrame(R); alg = :bottomup)
+      else vtree = nothing end
+      println("  Learning circuit...")
       C = learn_structured(R; binarize = true, n_projs = n_projs, t_proj = t_proj,
-                           max_height = max_height, trials = trials, dense_leaves = dense_leaves)
+                           max_height = max_height, trials = trials, dense_leaves = dense_leaves,
+                           split_t = split_t, vtree = vtree, residuals = residuals)
     else
       C = learn_projections(R; n_projs = n_projs, t_proj = t_proj, binarize = true, t_mix = t_mix,
                             max_height = max_height, trials = trials, dense_leaves = dense_leaves)
     end
+    out_data = open("logs/$(name)_dense_$(datasets[data_idx]).log", "w")
+    S[data_idx] = count_nodes(C, T)
+    tee(out_data, "Size: " * string(S[data_idx]))
+    tee(out_data, "LL: " * string(-NLL(C, T)))
     println("Learning parameters...")
     learner = SEM(C)
     indices = shuffle!(collect(1:size(R,1)))
-    out_data = open("logs/$(name)_dense_$(datasets[data_idx]).log", "w")
     tee(out_data, "Mini-batch EM...")
     avgnll = 0.0
     runnll = 0.0
@@ -84,9 +103,8 @@ function run_reg_em()
       end
     end
     LL[data_idx] = -NLL(C, T)
-    S[data_idx] = count_nodes(C, T)
-    tee(out, "LL: " * string(LL[data_idx]))
-    tee(out, "Size: " * string(S[data_idx]))
+    tee(out_data, "LL: " * string(LL[data_idx]))
+    tee(out_data, "Size: " * string(S[data_idx]))
     close(out_data)
   end
   println(LL)
