2de91d16656bae0196fc730322b9012baf7cdd7f
Renato Lui Geh <renatogeh@gmail.com>
2021-09-13 15:56:00 -0300

    Fix test_eval

--- test/test_eval.jl
+++ test/test_eval.jl
@@ -4,7 +4,7 @@
 
   C = Circuit(zoo_spn("nltcs"))
 
-  _, _, T_df = twenty_datasets("nltcs"; as_df = false)
+  _, _, T = twenty_datasets("nltcs"; as_df = false)
 
   r = -19582.020235794218
   @test isapprox(logpdf(C, T), r; atol = 100)
